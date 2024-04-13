af8883eb944c87aceb4e0133e4f713d84e8512a2
scnkera <scnkera@gmail.com>
2024-04-13 12:59:37 -0400

    Python using Karel

---
+++ karel.py
@@ -0,0 +1,54 @@
+
+def main():
+    move()
+    # your code here
+    collect_beepers()
+    move_to_wall()
+    collect_beepers()
+    move_to_wall()
+    collect_beepers()
+    move_to_wall()
+
+    
+    
+
+def collect_beepers():
+    while beepers_present():
+        pick_beeper()
+
+def move_to_wall():
+    if front_is_clear():
+        move()
+
+#def move_to_wall():
+    #while front_is clear():
+        #for i in range(10):
+            #pick_beeper()
+        #move()
+
+
+
+
+    def main():
+    move()
+    # your code here
+    collect_ten_beepers()
+    move()
+    move()
+    collect_ten_beepers()
+    move()
+    move()
+    collect_ten_beepers()
+
+def collect_ten_beepers():
+    while beepers_present():
+        pick_beeper()
+
+
+def detect_beeper
+    if beepers_present
+
+
+
+   
+   
